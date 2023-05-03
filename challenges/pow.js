655ec3fe22ac0d42f7d5ae236fb75ffe6384f6ec
Michael <mtrapani27@gmail.com>
2023-05-03 09:46:28 -0400

    5-3

---
+++ challenges/pow.js
@@ -0,0 +1,22 @@
+/** Write a function that calculates x^y, where x is given as the base and y is given as the power.
+ *
+ * Example:
+ * pow(2,4) => 2^4 = 16
+ * Rational: 2 * 2 * 2 * 2 = 16
+ *
+ * Extension: Use recursion
+ */
+
+function pow(base, power) {
+
+};
+
+/**
+ * Extension: Use recursion to solve the problem in O(n) time complexity -> Linear time complexity
+ */
+
+function powRecurse(base, power) {
+
+};
+
+module.exports = { pow, powRecurse };
